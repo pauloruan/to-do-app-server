62ea8743fff73eb9c919758a6c5a73ba96101797
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2022-08-16 18:56:10 -0300

    chore: Adcionado mensagens de erro.

--- src/helpers/messages.helper.ts
+++ src/helpers/messages.helper.ts
@@ -2,4 +2,11 @@ export const MessagesHelper = {
   PASSWORD_VALID:
     'A senha deve conter letras maiúsculas, minúsculas, números e caracteres especiais',
   PASSWORD_OR_EMAIL_INVALID: 'E-mail ou senha inválidos',
+  USER_ALREADY_EXISTS: 'Usuário já existe',
+  USER_NOT_CREATED: 'Usuário não criado',
+  USERS_NOT_FOUND: 'Nenhum usuário encontrado',
+  USER_NOT_FOUND: 'Usuário não encontrado',
+  TASK_NOT_CREATED: 'Tarefa não criada',
+  TASKS_NOT_FOUND: 'Nenhuma tarefa encontrada',
+  TASK_NOT_UPDATED: 'Tarefa não atualizada',
 };
