8d0d90d2236eae6cdb6c10ab4b4132e16f6481cd
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2022-08-18 17:17:48 -0300

    feat: Api documentada.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiTags } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 
+@ApiTags('Login')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
